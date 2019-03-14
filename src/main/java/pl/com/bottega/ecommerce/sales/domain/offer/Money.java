48dc96008c43606900364633dae70af130dafcb6
Oliwia Zarychta <209520@edu.p.lodz.pl>
2019-03-14 18:25:00 +0100

    Added Money class

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
@@ -0,0 +1,23 @@
+package pl.com.bottega.ecommerce.sales.domain.offer;
+
+import java.math.BigDecimal;
+
+public class Money {
+
+    private String currency;
+
+    private BigDecimal value;
+
+    public Money(String currency, BigDecimal value) {
+        this.currency = currency;
+        this.value = value;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+}
